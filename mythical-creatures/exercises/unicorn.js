66395d3e1233e38702f77e25fa1eac21ebd96733
Bret Merritt <merrittbret9@gmail.com>
2020-07-14 22:23:41 -0600

    Add unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,23 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
-    this.color = color;
+    this.color = color || 'white';
+  }
+  isWhite() {
+    if (this.color === 'white') {
+      return true;
+    } else {
+    return false;
+    }
+  }
+  says(text) {
+    return `**;* ${text} *;**`
   }
 }
 
 module.exports = Unicorn;
+
+// 1. read the test
+// 2. ask yourself if its going to fail and why
+// 3. run the test
+// 4. make it pass
